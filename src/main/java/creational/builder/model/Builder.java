f40b482bc3db2f4f873bb527625c7eed71142229
Pawel <pawel.matusiak29@gmail.com>
2023-02-06 12:29:00 +0100

    Builder with director

---
+++ src/main/java/creational/builder/model/Builder.java
@@ -0,0 +1,20 @@
+package creational.builder.model;
+
+
+public interface Builder {
+
+    Builder id(int id);
+
+    Builder brand(String brand);
+
+    Builder model(String model);
+
+    Builder color(String color);
+
+    Builder height(int height);
+
+    Builder engine(String engine);
+
+    Builder nbrOfDoors(int nbrOfDoors) ;
+
+}
