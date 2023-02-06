77deedb5679a7277227bf73cd24d54fe93ec12ec
Pawel <pawel.matusiak29@gmail.com>
2023-02-06 12:57:02 +0100

    Chain of responsibility

---
+++ src/main/java/behavioral/chainOfResponsibility/handlers/RoleCheckHandler.java
@@ -0,0 +1,15 @@
+package behavioral.chainOfResponsibility.handlers;
+
+public class RoleCheckHandler extends Handler {
+
+    @Override
+    public boolean handle(String username, String password) {
+        if ("admin_username".equals(username)) {
+            System.out.println("Loading Admin Page...");
+            return true;
+        }
+        System.out.println("Loading Default Page...");
+        return handleNext(username, password);
+    }
+
+}
---
+++ src/main/java/behavioral/chainOfResponsibility/handlers/UserExistsHandler.java
@@ -0,0 +1,23 @@
+package behavioral.chainOfResponsibility.handlers;
+
+import behavioral.chainOfResponsibility.Database;
+
+public class UserExistsHandler extends Handler {
+
+    private final Database database;
+
+    public UserExistsHandler(Database database) {
+        this.database = database;
+    }
+
+    @Override
+    public boolean handle(String username, String password) {
+        if (!database.isValidUser(username)) {
+            System.out.println("This username is not registered!");
+            System.out.println("Sign Up to our app now!");
+            return false;
+        }
+        return handleNext(username, password);
+    }
+
+}
